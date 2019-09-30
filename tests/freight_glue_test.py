8dd17851af20b6ec72a9d122bc6189ceacbd1d2b
James Lizamore <jwlizamore@gmail.com>
2019-09-30 10:47:36 +0100

    Add additional lambda test

--- tests/freight_glue_test.py
+++ tests/freight_glue_test.py
@@ -27,17 +27,32 @@ class TestFreightGlueSetup(unittest.TestCase):
     def test_iam_role(self):
         policy_dict = loads(self.result['virus_scanner']['aws_iam_role.virus_scan_role']['assume_role_policy'])
         self.assertEqual(policy_dict['Statement'][0]['Effect'], 'Allow')
+        self.assertEqual(policy_dict['Statement'][0]['Resource'], '*')
 
     def test_aws_iam_role_policy(self):
         policy_dict = loads(self.result['virus_scanner']['aws_iam_role_policy.virus_scan_policy']['policy'])
         self.assertEqual(policy_dict['Version'], '2012-10-17')
         self.assertEqual(policy_dict['Statement'][0]['Effect'], 'Allow')
 
+    def test_lambda_function(self):
+        self.assertEqual(
+            self.result['virus_scanner']['aws_lambda_function.virus_scanner_lambda']['handler'],
+            'update.lambda_handler'
+        )
+        self.assertEqual(
+            self.result['virus_scanner']['aws_lambda_function.virus_scanner_lambda']['runtime'],
+            'python2.7'
+        )
+        self.assertEqual(
+            self.result['virus_scanner']['aws_lambda_function.virus_scanner_lambda']['filename'],
+            'lambdas/lambda.zip'
+        )
+
     def test_destroy(self):
         self.assertEqual(self.result['virus_scanner']["aws_lambda_function.virus_scanner_lambda"]["destroy"], False)
 
     def test_destroy_tainted(self):
-        self.assertEqual(self.result['freight_glue']["aws_lambda_function.virus_scanner_lambda"]["destroy_tainted"], False)
+        self.assertEqual(self.result['virus_scanner']["aws_lambda_function.virus_scanner_lambda"]["destroy_tainted"], False)
 
 
 if __name__ == '__main__':
